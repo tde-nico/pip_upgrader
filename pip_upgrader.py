a787e052707a51a07999f84956958ca49972492e
tde-nico <101901057+tde-nico@users.noreply.github.com>
2022-05-10 08:36:12 +0200

    small_fix

--- pip_upgrader.py
+++ pip_upgrader.py
@@ -1,30 +1,37 @@
 import subprocess
 import os
 
-def main():
-    upgrades = 0
-    errors = 0
-    upgraded_libs = []
-    error_libs = []
-    os.system("py -m pip install -U pip")
-    libraries = subprocess.check_output('py -m pip list --outdated',shell=True).decode('cp1252')
-    libs = libraries.split('\n')[2:-1]
-    for lib in libs:
-        if os.system("py -m pip install -U " + lib.split(' ')[0]):
-            errors += 1
-            error_libs.append(lib)
-        else:
-            upgrades += 1
-            upgraded_libs.append(lib)
-    print('\n\n\n[+] Upgraded ' + str(upgrades) + ' libraries\n')
-    for lib in upgraded_libs:
-        print(lib)
-    if errors:
-        print('\n[-] Errors on ' + str(errors) + ' libraries\n')
-        for lib in error_libs:
-            print(lib)
-    input('\n'*5)
+python_command = "py"
+
+def upgrade():
+	upgrades, errors = [], []
+
+	os.system(python_command + " -m pip install -U pip")
+	libraries = subprocess.check_output(python_command + " -m pip list --outdated",shell=True).decode("cp1252")
+	libs = libraries.split('\n')[2:-1]
+
+	for lib in libs:
+		if os.system(python_command + " -m pip install -U " + lib.split(' ')[0]):
+			errors.append(lib)
+		else:
+			upgrades.append(lib)
+
+	return upgrades, errors
+
+
+
+def	main():
+	upgrades, errors = upgrade()
+
+	print('\n\n\n[+] Upgraded ' + str(len(upgrades)) + ' libraries\n')
+	for lib in upgrades:
+		print(lib)
+	if errors:
+		print('\n[-] Errors on ' + str(len(errors)) + ' libraries\n')
+		for lib in errors:
+			print(lib)
+	input("\n\n")
 
 
 if __name__ == '__main__':
-    main()
+	main()
